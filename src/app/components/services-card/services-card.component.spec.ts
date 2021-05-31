846f126105d3d0f860d9915f6b7d712061cda871
Alexis Caballero <acaballero@ludelaba.com.ar>
2021-05-30 23:54:44 -0300

    Actualizando Angular

--- src/app/components/services-card/services-card.component.spec.ts
+++ src/app/components/services-card/services-card.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ServicesCardComponent } from './services-card.component';
 
@@ -6,7 +6,7 @@ describe('ServicesCardComponent', () => {
   let component: ServicesCardComponent;
   let fixture: ComponentFixture<ServicesCardComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ServicesCardComponent ]
     })
--- src/app/pages/counts/counts.component.spec.ts
+++ src/app/pages/counts/counts.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CountsComponent } from './counts.component';
 
@@ -6,7 +6,7 @@ describe('CountsComponent', () => {
   let component: CountsComponent;
   let fixture: ComponentFixture<CountsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CountsComponent ]
     })
