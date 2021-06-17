f2ed772bbaabbb4404f856a821a333497a9ff604
javier <javimayer14@gmail.com>
2021-06-17 20:50:38 -0300

    ajustes en el login | ajustes generales

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/services/user/auth.service';
 import { Router } from '@angular/router';
 import swal from 'sweetalert2'; 
@@ -9,10 +10,11 @@ import swal from 'sweetalert2';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  isAuthenticated;
+  constructor(public route: ActivatedRoute, private authService:AuthService, private router:Router) { }
 
   ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
   }
 
 }
