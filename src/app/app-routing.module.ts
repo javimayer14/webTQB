b047beb50650c626103b61027fe529bc5e392ce8
Mayer <mayer@localhost.localdomain>
2021-11-18 15:39:58 -0300

    cambio en detalle del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
     component: TestInversorComponent,
   },
   {
-    path: 'projectDetail',
+    path: 'Sanmartin630',
     //component: ProjectDetailComponent, canActivate:[AuthGuard]
     component: ProjectDetailComponent,
   },
